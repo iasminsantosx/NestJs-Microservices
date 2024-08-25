ccd54aedd8d0c331b4ef91fc67ef26b4bfe077ec
iasminsantosx <iasminwinx@gmail.com>
2024-08-25 12:36:24 -0300

    fix conexão entre microserviços

--- tutorials-service/src/main.ts
+++ tutorials-service/src/main.ts
@@ -5,21 +5,9 @@ import { config } from 'dotenv';
 
 config();
 async function bootstrap() {
-  console.log('DB_HOST:', process.env.DB_HOST);
-  console.log('DB_PORT:', process.env.DB_PORT);
-  console.log('POSTGRES_USER:', process.env.POSTGRES_USER);
-  console.log('POSTGRES_PASSWORD:', process.env.POSTGRES_PASSWORD);
-  console.log('POSTGRES_DB:', process.env.POSTGRES_DB);
-  console.log('SENHA_JWT:', process.env.SENHA_JWT);
-  console.log('EXPIRE_TOKEN:', process.env.EXPIRE_TOKEN);
-  const app = await NestFactory.createMicroservice(TutorialsModule, {
-    transport: Transport.TCP,
-    options: {
-      port: 3001,
-    },
-  });
 
-  app.listen();
+  const app = await NestFactory.create(TutorialsModule);
+  await app.listen(3001);
 }
 
 bootstrap();
--- users-service/src/main.ts
+++ users-service/src/main.ts
@@ -1,18 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { UsersModule } from './users/users.module';
-import { Transport } from '@nestjs/microservices';
 import { config } from 'dotenv';
 
 config();
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(UsersModule, {
-    transport: Transport.TCP,
-    options: {
-      port: 3000,
-    },
-  });
-
-  app.listen();
+  const app = await NestFactory.create(UsersModule);
+  await app.listen(3000);
 }
 
 bootstrap();
